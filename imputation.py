b8d2bcfec4a8f90535ff0bc7eca1cf8432ddd734
Nikita Albert <nikitaalbert@gmail.com>
2025-03-20 19:55:22 -0500

    fixing how imputed values overwrite to dataframe.

--- imputation.py
+++ imputation.py
@@ -6,7 +6,7 @@ data_columns = ['ID', 'Name', 'Age', 'Height', 'Weight', 'Eye_Color']
 data = [[1,'Harry',23,172,65,None],
         [2,'Sally',45,None,75,'Green'], 
         [3,'Sabrina',31,148,58,'Brown'],
-        [4,'Alex',29,184,68,],
+        [4,'Alex',29,184,68,None],
         [5,'Jon',37,183,70,'Brown']]
 
 input_df = pd.DataFrame(data=data, columns=data_columns)
@@ -18,21 +18,17 @@ input_df['Eye_Color'] = input_df['Eye_Color'].fillna(input_df['Eye_Color'].mode(
 print(input_df.head(), "\n")
 
 # Here we select features for KNN imputation, which are "Weight" and "Age"
-X = input_df[['Weight', 'Age']]
+X = input_df[['Weight', 'Age', 'Height']]
 # Here we select the target column, "Height"
 y = input_df['Height']
 
 # Identify rows with missing values in Height
-missing_height_indices = y.isnull()
+#missing_height_indices = y.isnull()
 
-knn_imputer = KNNImputer(n_neighbors=5, weights="uniform", metric='nan_euclidean')
+knn_imputer = KNNImputer(n_neighbors=5, weights="uniform", metric='nan_euclidean', copy=True)
 
-knn_imputer.fit(X.dropna())
+input_df[['Weight', 'Age', 'Height']] = knn_imputer.fit_transform(X, y)
 
-imputed_df = pd.DataFrame(index=range(5), columns=["Height"])
-
-imputed_df['Height'] = knn_imputer.transform(X)
-
-input_df.loc[missing_height_indices, "Height"] = imputed_df.loc[missing_height_indices, "Height"]
+#input_df.loc[missing_height_indices, "Height"] = imputed_df.loc[missing_height_indices, "Height"]
 
 print(input_df)
